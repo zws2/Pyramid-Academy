75f454db200cecb195166b6d384590c621b65923
Zachary Snyder <snyderzw@hotmail.com>
2021-09-02 11:42:11 -0500

    selects a random number for you to guess

--- Guess-The-Number/src/com/company/Main.java
+++ Guess-The-Number/src/com/company/Main.java
@@ -1,15 +1,23 @@
 package com.company;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+
         System.out.println("Hello! What is your name?");
 
         String name = scanner.nextLine();
 
-        System.out.println(name);
+        System.out.println("Well, Abaddon, I am thinking of a number between 1 and 20.");
+
+        int guess, number;
+        number = rand.nextInt(20) + 1;
+
+        System.out.println(number);
     }
 }
