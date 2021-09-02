eb791973b1d042a128d0adec3048bcf7263625ed
Zachary Snyder <snyderzw@hotmail.com>
2021-09-02 11:55:26 -0500

    asks for guesses until correct or no more guesses remain

--- Guess-The-Number/src/com/company/Main.java
+++ Guess-The-Number/src/com/company/Main.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        final int MAX_GUESSES = 6;
+
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
 
@@ -15,9 +17,23 @@ public class Main {
 
         System.out.println("Well, Abaddon, I am thinking of a number between 1 and 20.");
 
-        int guess, number;
+        int guess = 0, number = 0, counter = 0;
         number = rand.nextInt(20) + 1;
 
         System.out.println(number);
+
+        while(guess != number && counter < MAX_GUESSES){
+            System.out.println("Take a Guess.");
+            try{
+                guess = scanner.nextInt();
+                counter++;
+            }catch(Exception e){
+                System.out.println("That was not a number.");
+            }
+        }
+
+        if(counter <= MAX_GUESSES) System.out.println("Good job, Abaddon! You guessed my number in 3 guesses!");
+        else System.out.println("You ran out of guesses");
+
     }
 }
