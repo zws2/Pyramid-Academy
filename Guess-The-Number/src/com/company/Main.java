dc3b1aef057f9d1ae061fbf81d8889599da02693
Zachary Snyder <snyderzw@hotmail.com>
2021-09-02 12:05:45 -0500

    added play again option

--- Guess-The-Number/src/com/company/Main.java
+++ Guess-The-Number/src/com/company/Main.java
@@ -15,25 +15,38 @@ public class Main {
 
         String name = scanner.nextLine();
 
-        System.out.printf("Well, %s, I am thinking of a number between 1 and 20.\n", name);
+        String playAgain = "y";
 
-        int guess = 0, number = 0, counter = 0;
-        number = rand.nextInt(20) + 1;
+        while(playAgain.equals("y")){
+            System.out.printf("Well, %s, I am thinking of a number between 1 and 20.\n", name);
 
-        System.out.println(number);
+            int guess = 0, number = 0, counter = 0;
+            number = rand.nextInt(20) + 1;
 
-        while(guess != number && counter < MAX_GUESSES){
-            System.out.println("Take a Guess.");
+            System.out.println(number);
+
+            while(guess != number && counter < MAX_GUESSES){
+                System.out.println("Take a Guess.");
+                try{
+                    guess = scanner.nextInt();
+                    counter++;
+                }catch(Exception e){
+                    System.out.println("That was not a number.");
+                }
+            }
+
+            if(counter <= MAX_GUESSES) System.out.printf("Good job, %s! You guessed my number in %d guesses!\n", name, counter);
+            else System.out.println("You ran out of guesses");
+
+            System.out.println("Would you like to play again? (y or n)");
             try{
-                guess = scanner.nextInt();
-                counter++;
+                scanner.nextLine();
+                playAgain = scanner.nextLine();
             }catch(Exception e){
-                System.out.println("That was not a number.");
+                System.out.println("Invalid input.");
             }
         }
 
-        if(counter <= MAX_GUESSES) System.out.printf("Good job, %s! You guessed my number in %d guesses!\n", name, counter);
-        else System.out.println("You ran out of guesses");
 
     }
 }
