ae5de68bc67263043d02a00a09007e6a9c4cc2b2
Zachary Snyder <snyderzw@hotmail.com>
2021-09-02 11:37:56 -0500

    asks for name and accepts scanner input

--- Guess-The-Number/src/com/company/Main.java
+++ Guess-The-Number/src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello! What is your name?");
 
+        String name = scanner.nextLine();
+
+        System.out.println(name);
     }
 }
