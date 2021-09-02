7663787321d09aa42fe3c88347918607d211f953
Zachary Snyder <snyderzw@hotmail.com>
2021-09-02 17:02:02 -0500

    added exit conditions and comments

--- Hangman/src/com/company/Main.java
+++ Hangman/src/com/company/Main.java
@@ -4,22 +4,26 @@ import java.util.Scanner;
 
 public class Main {
 
-    static String guessed_letters = "";
-    static String codeword = "cat";
+    static String guessed_letters = "";//string that tracks previously guessed letters
+    static String codeword = "cat";//codeword for the user to guess
 
-    static int num_wrong_guesses = 0;
-    static int num_letters_revealed = 0;
+    static int num_letters_revealed = 0;//how many letters in the codeword have been guessed
+    static int num_wrong_guesses = 0;//number of times user has input a letter that was not in codeword
+
+    static final int MAX_WRONG_GUESSES = 6;//amount of times player can guess incorrectly
 
     public static void main(String[] args) {
         playHangman();
     }
 
+    //this function initiates the game of hangman taking scanner input
+    //and looping until an end condition is met.
     private static void playHangman(){
 
         Scanner scanner = new Scanner(System.in);
         String input;
 
-        while(num_wrong_guesses<6){
+        while(num_wrong_guesses < MAX_WRONG_GUESSES){
             drawHangman();
             System.out.println("Please guess a letter:");
 
@@ -36,14 +40,18 @@ public class Main {
                     System.out.println("Input a single character only.");
                 }
             }
-
             System.out.println(revealedCodeword());
+            if(num_letters_revealed == codeword.length()){//You win
+                System.out.println("Hooray! You win!");
+                return;
+            }
         }
-        if(num_letters_revealed == codeword.length()){
-            System.out.println("Hooray!");
-        }else System.out.println("You lose!");
+        //You lose
+        drawHangman();
+        System.out.println("You lose!");
     }
 
+    //this function draws the hangman stick figure
     private static void drawHangman(){
 
         System.out.printf("missed Letters: %s\n", guessed_letters);
@@ -102,22 +110,24 @@ public class Main {
         }
     }
 
+    //this function computes the revealed codeword based on previously guessed letters
+    //it sets the instance variables num_letters_revealed and num_wrong_guesses depending on the result.
     private static String revealedCodeword(){
-        String str = "";
-
-        int temp_num_letters_revealed = 0;
+        String str = "";//string with the characters that make up the revealed codeword
+        int temp_num_letters_revealed = 0;//a temp variable to compare to the previous amount of letters revealed
 
         try{
+            //loops through each letter in the codeword and see if it is contained within the letters guessed
             for (int i = 0; i < codeword.length(); i++) {
                 String current_letter = codeword.substring(i,i+1);
 
-                if(guessed_letters.contains(current_letter)){
+                if(guessed_letters.contains(current_letter)){//add that letter to the string
                     temp_num_letters_revealed++;
                     str = str + current_letter;
-                } else str = str + "_";
+                } else str = str + "_";//add a _ placeholder
             }
 
-            if(temp_num_letters_revealed == num_letters_revealed) num_wrong_guesses++;
+            if(temp_num_letters_revealed == num_letters_revealed) num_wrong_guesses++;//if no new letters were revealed, the guess was wrong.
             else num_letters_revealed = temp_num_letters_revealed;
 
         }catch(Exception e){
