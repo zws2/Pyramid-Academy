6952f8baf16ee913e6f85d6ba675373018667277
Zachary Snyder <snyderzw@hotmail.com>
2021-09-04 17:12:09 -0500

    added win checking for rows and colls

--- Tic-Tac-Toe/src/com/company/Main.java
+++ Tic-Tac-Toe/src/com/company/Main.java
@@ -7,6 +7,8 @@ public class Main {
     private static char[] board;
     private static char player_token = ' ';
 
+    final static int BOARD_SIZE = 9;
+
     public static void main(String[] args) {
 	// write your code here
 
@@ -25,10 +27,8 @@ public class Main {
         do{
             input = scanner.nextLine();
         }while(!input.matches("[xoXO]"));
-        input.toUpperCase();
+        input = input.toUpperCase();
         player_token = input.charAt(0);
-
-        System.out.println(player_token);
 
         do{
             drawBoard();
@@ -37,13 +37,43 @@ public class Main {
             }while(!input.matches("[1-9]"));
             board[Integer.parseInt(input)-1] = player_token;
 
-        }while(isBoardFull());
+        }while(!isBoardFull() && !hasWon('X') && !hasWon('O'));
         drawBoard();
         System.out.println("Game over");
     }
 
+    private static boolean hasWon(char mark){
+
+        final int BOARD_LEN = 3;
+
+        //check row
+        int start = 0;
+        for (int i = start; i < BOARD_SIZE; i++) {
+            if(board[i] != mark){
+                start += BOARD_LEN;
+                i = start;
+            }else if(i == start + BOARD_LEN-1){
+                System.out.println("row win for " + mark);
+                return true;
+            }
+        }
+
+        //check col
+        start = 0;
+        for (int i = start; i < BOARD_SIZE; i+=BOARD_LEN) {
+            if(board[i] != mark){
+                start++;
+                i = start;
+            }else if(i == BOARD_SIZE-BOARD_LEN+start){
+                System.out.println("row win for " + mark);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     private static void initBoard(){
-        final int BOARD_SIZE = 9;
         board = new char[BOARD_SIZE];
         for (int i = 0; i < BOARD_SIZE; i++) {
             board[i] = ' ';
@@ -51,11 +81,10 @@ public class Main {
     }
 
     private static boolean isBoardFull(){
-        final int BOARD_SIZE = 9;
         for (int i = 0; i < BOARD_SIZE; i++) {
-            if(board[i] == ' ') return true;
+            if(board[i] == ' ') return false;
         }
-        return false;
+        return true;
     }
 
     private static void drawBoard(){
