eab5ecd037bf2a91e7a2fa3215b45d6b1a3cbe86
Zachary Snyder <snyderzw@hotmail.com>
2021-09-04 16:01:18 -0500

    accepts input for player_token

--- Tic-Tac-Toe/src/com/company/Main.java
+++ Tic-Tac-Toe/src/com/company/Main.java
@@ -1,8 +1,47 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static char[] board;
+    private static char player_token = ' ';
 
     public static void main(String[] args) {
 	// write your code here
+
+        initGame();
+    }
+
+    private static void initGame(){
+
+        Scanner scanner = new Scanner(System.in);
+        initBoard();
+
+        System.out.print("Welcome to Tic-Tac-Toe!\n" +
+                "Do you want to be X or O?\n");
+
+        String input;
+        do{
+            input = scanner.nextLine();
+        }while(!input.matches("[xoXO]"));
+        input.toUpperCase();
+        player_token = input.charAt(0);
+
+        System.out.println(player_token);
+
+
+
+    }
+
+    private static void initBoard(){
+        board = new char[9];
+        for (char c : board) {
+            c = ' ';
+        }
+    }
+
+    private static void drawBoard(){
+
     }
 }
