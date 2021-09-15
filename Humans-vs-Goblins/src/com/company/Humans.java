4d43f3101f61c2de2061de63a9717584644f8b85
Zachary Snyder <snyderzw@hotmail.com>
2021-09-15 14:51:15 -0500

    added goblins, humans, and land classes

---
+++ Humans-vs-Goblins/src/com/company/Humans.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Humans {
+
+    public String toString(){
+        return "";
+    }
+}
--- Humans-vs-Goblins/src/com/company/Main.java
+++ Humans-vs-Goblins/src/com/company/Main.java
@@ -2,7 +2,20 @@ package com.company;
 
 public class Main {
 
+    public char[][] grid;
+    public final int gridSize = 10;
+
     public static void main(String[] args) {
 	// write your code here
+
+
+    }
+
+    public static void init(){
+//        for (int i = 0; i < gridSize; i++) {
+//
+//
+//
+//        }
     }
 }
