47ee5a7e34caea918804f16e2b6a08738121a376
Zachary Snyder <snyderzw@hotmail.com>
2021-09-15 14:55:35 -0500

    added display and init grid methods

--- Humans-vs-Goblins/src/com/company/Main.java
+++ Humans-vs-Goblins/src/com/company/Main.java
@@ -2,20 +2,33 @@ package com.company;
 
 public class Main {
 
-    public char[][] grid;
-    public final int gridSize = 10;
+    public static char[][] grid;
+    public static final int GRID_SIZE = 10;
 
     public static void main(String[] args) {
 	// write your code here
 
+        initGrid();
+        displayGrid();
+
 
     }
 
-    public static void init(){
-//        for (int i = 0; i < gridSize; i++) {
-//
-//
-//
-//        }
+    public static void initGrid(){
+        grid = new char[GRID_SIZE][GRID_SIZE];
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                grid[i][j] = ' ';
+            }
+        }
     }
+    public static void displayGrid(){
+        for (int i = 0; i < GRID_SIZE; i++) {
+            for (int j = 0; j < GRID_SIZE; j++) {
+                System.out.print("|"+grid[i][j]);
+            }
+            System.out.println("|");
+        }
+    }
+
 }
