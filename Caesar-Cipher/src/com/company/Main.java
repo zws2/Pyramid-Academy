def1b5b2cc2442bad573dac6506baff0a97eacb0
Zachary Snyder <snyderzw@hotmail.com>
2021-09-21 15:43:21 -0500

    added decryption and fixed encryption

--- Caesar-Cipher/src/com/company/Main.java
+++ Caesar-Cipher/src/com/company/Main.java
@@ -23,12 +23,11 @@ public class Main {
         do {
 
             mode = askForMode();
-            if(!mode.equals("!exit")) break;
-            System.out.println(mode);
+            if(mode.equals("!exit")) break;
             message = askForMessage();
             shift = askForShift();
 
-            System.out.println("Your translated text is: \n" + caesarCipher(message, shift));
+            System.out.println("Your translated text is: \n" + caesarCipher(mode, message, shift));
 
         }while(true);
         scanner.close();
@@ -60,32 +59,40 @@ public class Main {
     private static int askForShift(){
         String input = "";
         do {
-            System.out.println("Enter the key number (1-52)");
+            System.out.println("Enter the key number (1-51)");
             try{
                 input = scanner.nextLine();
             }catch(Exception ignored){}
-        } while (!input.matches("5[0-2]|[1-4][0-9]|[1-9]"));
+        } while (!input.matches("5[0-1]|[1-4][0-9]|[1-9]"));
 
         return Integer.parseInt(input);
     }
 
-    private static String caesarCipher(String message, int shift){
+    private static String caesarCipher(String mode, String message, int shift){
+
+        if(mode.equals("decrypt") || mode.equals("d")){
+            shift = 52 - shift;
+        }
 
         StringBuilder output = new StringBuilder();
+        //26 * 2 = 52
         //A-Z 65-90
         //a-z 97-122
+        //ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz
 
         for (int i = 0; i < message.length(); i++) {
             char c = message.charAt(i);
+            char nc = message.charAt(i);
             if((c >= 65 && c <= 90) || (c >= 97 && c <= 122)){
-                c += shift;
-                if(c > 90 && c < 97) c += 6;
-                else if(c > 122){
-                    c -= 52;
-                    if(c > 90 && c < 97) c += 6;
+                if(c <= 90 && c+shift > 90) nc += 6 + shift;
+                else nc+= shift;
+
+                if(nc > 122){
+                    nc -= 58;
+                    if(nc > 90) nc+= 6;
                 }
             }
-            output.append(c);
+            output.append(nc);
         }
 
         return output.toString();
