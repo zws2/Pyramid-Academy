5096b5f0ed39e1a615f3ab633a31e8e8e256fecf
Zachary Snyder <snyderzw@hotmail.com>
2021-10-06 23:42:56 -0500

    hangman lisp

--- code/lisp/hangman/src/hangman/core.clj
+++ code/lisp/hangman/src/hangman/core.clj
@@ -1,86 +1,92 @@
-(ns hangman.core)
+(ns hangman.core
+  (:require [clojure.string :as str]))
 
-(defn hangman [])
-(defn printHangman [numWrong]
+
+(def word-progress "")
+(def missed-letters "")
+(def flag false)
+
+(defn print-hangman [num-wrong]
   (cond
-    (= numWrong 0) (print   " +--+\n"
-                            "    |\n"
-                            "    |\n"
-                            "    |\n"
-                            "____|\n")
-    (= numWrong 1) (println " +--+\n"
-                            " O  |\n"
-                            "    |\n"
-                            "    |\n"
-                            "____|\n")
-    (= numWrong 2) (println " +--+\n"
-                            " O  |\n"
-                            " |  |\n"
-                            "    |\n"
-                            "____|\n")
-    (= numWrong 3) (println " +--+\n"
-                            " O  |\n"
-                            " |  |\n"
-                            " |  |\n"
-                            "____|\n")
-    (= numWrong 4) (println " +--+\n"
-                            " O  |\n"
-                            " |\ |\n"
-                            " |  |\n"
-"____|\n")
-    (= numWrong 5) (println " +--+\n"
-                            " O  |\n"
-                            "/|\ |\n"
-                            " |  |\n"
-                            "____|\n"))
+    (= num-wrong 0) (print "  +--+\n"
+                           "    |\n"
+                           "    |\n"
+                           "    |\n"
+                           "____|\n")
+    (= num-wrong 1) (println "  +--+\n"
+                             " O  |\n"
+                             "    |\n"
+                             "    |\n"
+                             "____|\n")
+    (= num-wrong 2) (println "  +--+\n"
+                             " O  |\n"
+                             " |  |\n"
+                             "    |\n"
+                             "____|\n")
+    (= num-wrong 3) (println "  +--+\n"
+                             " O  |\n"
+                             " |  |\n"
+                             " |  |\n"
+                             "____|\n")
+    (= num-wrong 4) (println "  +--+\n"
+                             " O  |\n"
+                             " |\\ |\n"
+                             " |  |\n"
+                             "____|\n")
+    (= num-wrong 5) (println "  +--+\n"
+                             " O  |\n"
+                             "/|\\ |\n"
+                             " |  |\n"
+                             "____|\n")
+    (= num-wrong 6) (println "  +--+\n"
+                             " O  |\n"
+                             "/|\\ |\n"
+                             "/|  |\n"
+                             "____|\n")))
 
-    ))
-;
-;case 0: System.out.print(
-;                          " +--+\n" +
-;                          "    |\n" +
-;                          "    |\n" +
-;                          "    |\n" +
-;                          "____|\n");
-;break;
-;case 1: System.out.print(
-;                          " +--+\n" +
-;                          " O  |\n" +
-;                          "    |\n" +
-;                          "    |\n" +
-;                          "____|\n");
-;break;
-;case 2: System.out.print(
-;                          " +--+\n" +
-;                          " O  |\n" +
-;                          " |  |\n" +
-;                          "    |\n" +
-;                          "____|\n");
-;break;
-;case 3: System.out.print(
-;                          " +--+\n" +
-;                          " O  |\n" +
-;                          " |  |\n" +
-;                          " |  |\n" +
-;                          "____|\n");
-;break;
-;case 4: System.out.print(
-;                          " +--+\n" +
-;                          " O  |\n" +
-;                          " |\\ |\n" +
-;                          " |  |\n" +
-;                          "____|\n");
-;break;
-;case 5: System.out.print(
-;                          " +--+\n" +
-;                          " O  |\n" +
-;                          "/|\\ |\n" +
-;                          " |  |\n" +
-;                          "____|\n");
-;break;
-;case 6: System.out.print(
-;                          " +--+\n" +
-;                          " O  |\n" +
-;                          "/|\\ |\n" +
-;                          "/|  |\n" +
-;                          "____|\n");
+
+
+(defn ask-for-guess []
+  (println "Guess a letter")
+  (let [letter (read-line)]
+    letter))
+
+
+(defn calculate-progress [codeword guess]
+  (def flag false)
+  (loop [n 0]
+    (let [letter (nth codeword n)]
+      (def word-progress
+        (if (= guess (format "%s" letter))
+          (do
+            (def flag true)
+            (apply str (assoc (vec word-progress) n letter)))
+          word-progress)))
+    (when (< n (- (count codeword) 1))
+      (recur (+ n 1))))
+  (if (not flag)
+    (def missed-letters (format "%s%s" missed-letters guess)))
+  (println word-progress))
+
+(defn print-game-status []
+  (println "Missed letters: " missed-letters)
+  (println word-progress))
+
+
+(defn hangman []
+
+  (print-hangman 0)
+  (let [codeword "hangman"]
+    (loop [n 0]
+      (def word-progress (format "%s%s" word-progress "_"))
+      (when (< n (- (count codeword) 1)) (recur (+ n 1))))
+    (loop []
+      (print-game-status)
+      (calculate-progress codeword (ask-for-guess))
+      (print-hangman (count missed-letters))
+      (when (and (< (count missed-letters) 6) (not= word-progress codeword)) (recur)))
+    (if (>= (count missed-letters) 6)
+      (println "You lose!")
+      (println "You win!"))
+    (println "The secret word was" codeword)))
+(hangman)
